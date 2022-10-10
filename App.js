a644b46f6ea71b9ad57780cc2611dde724e6cc50
gheysiell <gheysiellfarukzinn@hotmail.com>
2022-10-10 11:27:39 -0400

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { View, StyleSheet, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [text, setText] = useState('Digite alguma coisa na caixa de texto')
+
+    const speak = () => {
+        Speech.speak(text, {
+            language: 'pt-BR'
+        })
+    }
+
+    const handleText = (e) => {
+        setText(e)
+
+        if (e === '') {
+            setText('Digite alguma coisa na caixa de texto')
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <TextInput 
+                style={styles.input}
+                onChangeText={e => handleText(e)}
+                placeholder="Digite alguma coisa..."
+            />
+            <Button
+                title='Falar'
+                onPress={speak}
+                style={styles.button}
+            />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        height: 40,
+        width: 100,
+        backgroundColor: 'blue',
+        borderRadius: 10,
+        marginTop: 20,
+    },
+    input: {
+        width: '90%',
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#121212',
+        borderRadius: 10,
+        padding: 10,
+        margin: 10,
+        color: '#000',
+        fontSize: 20,
+    },
+})
---
+++ README.md
@@ -0,0 +1,5 @@
+# text_to_speech
+
+<div> <h2> App that translates text to speech, made with react native / expo-speech </h2> </div>
+<div> <h5> ° For instaling dependences, npm install </h5> </div>
+<div> <h5> ° For init project go into the same folder, and run npm start </h5> </div>
